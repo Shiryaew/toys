1b986bac57e9dafc3ef569b791357e369caa60c0
Vaultjedi <vaultboy13vaultboy13@gmail.com>
2022-11-08 20:14:19 +0300

    Auth,footer,header pages. Updated web path
    Signed-off-by: Vaultjedi <vaultboy13vaultboy13@gmail.com>

---
+++ resources/views/header.blade.php
@@ -0,0 +1,44 @@
+</html>
+<title>Toys</title>
+        <style>
+            html {
+                height: 100%;
+                width: 100%;
+            }
+            body {
+                margin: 0;
+                height: 200%;
+                width: 100%;
+                background-color: lightgray;
+            }
+            .header {
+                position: sticky;
+                top: 0;
+                width: 100%;
+                height: 100px;
+                background-color: white;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                box-shadow: 0 5px 5px gray;
+            }
+            
+            .header__name {
+                color: black;
+                font-size: 36px;
+
+            }
+           
+
+            
+
+        </style>
+		<body>
+		<div class="header">
+            <div class="header__name">
+                Производство детских игрушек
+            </div>
+
+        </div>
+		</body>
+</html>
---
+++ resources/views/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Toys</title>
+        <style>
+            html {
+                height: 100%;
+                width: 100%;
+            }
+            body {
+                margin: 0;
+                height: 200%;
+                width: 100%;
+                background-color: lightgray;
+            }
+            .header {
+                position: sticky;
+                top: 0;
+                width: 100%;
+                height: 100px;
+                background-color: white;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                box-shadow: 0 5px 5px gray;
+            }
+            
+            .header__name {
+                color: black;
+                font-size: 36px;
+
+            }
+            .btn {
+                margin: 10px;
+                margin-bottom: 0;
+            }
+            .btn__container {
+                height: 100%;
+                border: 1px solid;
+                display: flex;
+                width: 200px;
+                flex-direction: column;
+                background-color: white;
+            }
+
+            
+
+        </style>
+    </head>
+    <body>
+        <div class="header">
+            <div class="header__name">
+                Производство детских игрушек
+            </div>
+
+        </div>
+        <div class="btn__container">
+            <button class="btn btn__first">
+                <div>
+                    Первая кнопка
+                </div>
+            </button>
+            <button class="btn btn__second">
+                <div>
+                    Вторая кнопка
+                </div>
+            </button>
+
+        </div>
+            
+
+    </body>
+</html>
