88742fc2a8280bdcefc2b8232e6305e38fcb909b
Vaultjedi <vaultboy13vaultboy13@gmail.com>
2022-11-22 18:04:53 +0300

    Created Agent,Product pages, controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ToyController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get('/auth', function () {
+    return view('auth');
+});
+Route::get('product',[ToyController::class,'index']);
